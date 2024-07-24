feef068b44549b6c559c0bc893d5fd3ea24295f4
S P Kaveri <90822455+KaveriSP@users.noreply.github.com>
2024-07-24 14:12:11 +0530

    Add files via upload

---
+++ QuantumChat/QuantumChat/javascript/signup.js
@@ -0,0 +1,28 @@
+const form = document.querySelector(".signup form"),
+continueBtn = form.querySelector(".button input"),
+errorText = form.querySelector(".error-text");
+
+form.onsubmit = (e)=>{
+    e.preventDefault();  //preventing form from submitting
+}
+
+continueBtn.onclick = ()=>{
+  //let's start Ajax
+    let xhr = new XMLHttpRequest(); //creating XML object
+    xhr.open("POST", "php/signup.php", true);
+    xhr.onload = ()=>{
+      if(xhr.readyState === XMLHttpRequest.DONE){
+          if(xhr.status === 200){
+              let data = xhr.response;
+              if(data === "success"){
+                location.href="users.php";
+              }else{
+                errorText.style.display = "block";
+                errorText.textContent = data;
+              }
+          }
+      }
+    }
+    let formData = new FormData(form);
+    xhr.send(formData);
+}
